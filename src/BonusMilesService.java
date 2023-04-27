9b35e15d1feb4171ed7899b4c4b724fb0199a8c5
RytoryQA <vikaryzhkina89@gmail.com>
2023-04-27 16:04:52 +0400

    Revision

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,9 +2,9 @@ public class BonusMilesService {
     public int calculate(int price) {
         int oneMile = 20;
         int result;
-        if (5 > 2) {
+
             result = price / oneMile;
-        }
+
         return result;
     }
 }
